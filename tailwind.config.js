7b0dcf4e2e16d461d76a5c33c70c99cf931f4db7
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-10-11 00:30:27 +0600

    refactor: remove reportWebVitals file and its usage in index.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js, jsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx}', './public/index.html'],
   darkMode: 'class',
   theme: {
     extend: {},
