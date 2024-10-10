b3708f93f12fe0659c867b83056b6169d1beefa2
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-10-11 00:11:49 +0600

    chore: configure Tailwind CSS with dark mode enabled

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js, jsx}', './public/index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
