45ba387fcc30af6d6bd93af716023a25bf7a2629
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-10-20 03:45:14 +0600

    feat: add getCurrentWeather to fetch the current weather data based on city and unit

---
+++ src/services/weatherService.js
@@ -0,0 +1,35 @@
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+const getLatLon = async (city) => {
+  const apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`;
+  try {
+    const response = await fetch(apiURL);
+    const data = response.json();
+    if (data.length > 0) {
+      const lat = data[0]["lat"];
+      const lon = data[0]["lon"];
+      console.log("City found");
+      return { lat, lon };
+    } else {
+      console.log("City not found");
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getCurrentWeather = async (city, unit) => {
+  const location = await getLatLon(city);
+  if (location) {
+    const { lat, lon } = location;
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}.15&appid=${API_KEY}&units=${unit}`;
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    return data;
+  } else {
+    return null;
+  }
+};
+
+export {getCurrentWeather};
