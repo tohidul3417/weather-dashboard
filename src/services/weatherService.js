5c49ae9dcad8a5a6d0e87f3d453817e87fe223d1
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-10-20 18:41:51 +0600

    fix(WeatherService): use HTTPS for API URLs and remove key logging

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,8 +1,7 @@
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-console.log('API Key', API_KEY);
 
 const getLatLon = async (city) => {
-  const apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`;
+  const apiURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`;
   try {
     const response = await fetch(apiURL);
     const data = await response.json();
