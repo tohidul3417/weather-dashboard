727d019357248d6fd3c55352f4dc012a780a75a5
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-10-20 14:30:38 +0600

    refactor: remove SearchBar, ForecastCard, and useEffect from Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,14 @@
 import React, {useContext, useEffect} from "react";
-import SearchBar from "../components/SearchBar";
 import WeatherCard from "../components/WeatherCard";
-import ForecastCard from "../components/ForecastCard";
+// import ForecastCard from "../components/ForecastCard";
 import { WeatherContext } from "../context/WeatherContext";
 
 const Home = () => {
-    const {forecast, fetchWeather} = useContext(WeatherContext);
-
-    useEffect(() => {
-        // Fetch weather for a default location on load
-        fetchWeather('Dhaka');
-
-    }, [fetchWeather]);
+    const {fetchWeather} = useContext(WeatherContext);
 
     return (
         <div>
-            <SearchBar />
             <WeatherCard />
-            <div>
-                {forecast.map(day => (
-                    <ForecastCard key={day.dt} day={day} />
-                ))}
-            </div>
         </div>
     )
 }
