32f62db8fb05f7c4a0d9a92a9a1603919a153d85
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-10-22 16:36:16 +0600

    style(Header): add Tailwind CSS classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,15 @@ import UnitToggle from "./UnitToggle";
 
 const Header = () => {
     return (
-        <header>
-            <h1>Weather Dashboard</h1>
-            <div>
+        <header className="bg-blue-500">
+            <div className="max-w-7xl mx-auto text-white p-4 flex flex-col md:flex-row items-center justify-between">
+            <h1 className="text-2xl font-bold">Weather Dashboard</h1>
+            <div className="mt-4 md:mt-0">
                 <SearchBar />
                 <UnitToggle />
             </div>
+            </div>
+           
         </header>
     )
 };
