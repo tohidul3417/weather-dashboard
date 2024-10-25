9b8d079127d217f8bcaf51d135770811810f3272
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-10-25 13:11:35 +0600

    style(Header): add Tailwind CSS classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,17 @@ import SearchBar from "./SearchBar";
 import UnitToggle from "./UnitToggle";
 
 const Header = () => {
-    return (
-        <header className="bg-blue-500">
-            <div className="max-w-7xl mx-auto text-white p-4 flex flex-col md:flex-row items-center justify-between">
-            <h1 className="text-2xl font-bold">Weather Dashboard</h1>
-            <div className="mt-4 md:mt-0">
-                <SearchBar />
-                <UnitToggle />
-            </div>
-            </div>
-           
-        </header>
-    )
+  return (
+    <header className="bg-blue-500">
+      <div className="max-w-7xl mx-auto text-white p-4 flex flex-col md:flex-row items-center justify-between">
+        <h1 className="text-2xl font-bold">Weather Dashboard</h1>
+        <div className="mt-4 flex flex-col md:flex-row">
+          <SearchBar />
+          <UnitToggle />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
