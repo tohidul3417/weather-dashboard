b64fb223f52a6c6f740a828262147a462fac31cf
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-10-27 00:30:49 +0600

    feat(Header): add active style to Favorites link for improved navigation visibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import UnitToggle from "./UnitToggle";
 
@@ -9,12 +9,22 @@ const Header = () => {
       <div className="max-w-7xl md:mx-auto text-white p-4 flex flex-col md:flex-row md:items-center justify-between">
         <div className="flex items-center justify-between space-x-4">
           <Link to="/">
-            <h1 className="text-2xl font-bold hover:underline">Weather Dashboard</h1>
+            <h1 className="text-2xl font-bold">
+              Weather Dashboard
+            </h1>
           </Link>
           <nav>
-          <Link to="/favorites" className="text-lg hover:underline">Favorites</Link>
+            <NavLink
+              to="/favorites"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-lg text-blue-800 bg-yellow-400 font-semibold p-1 rounded "
+                  : "text-lg"
+              }
+            >
+              Favorites
+            </NavLink>
           </nav>
-          
         </div>
 
         <div className="mt-4 md:mt-0 flex flex-col md:flex-row">
