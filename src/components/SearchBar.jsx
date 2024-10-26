dfb801df549e15df56028e6c84f599cf4706de01
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-10-27 01:07:37 +0600

    feat(SearchBar): add navigation to Home page on search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useContext } from "react";
 import { WeatherContext } from "../context/WeatherContext";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
   const [city, setCity] = useState("");
   const { handleCityName } = useContext(WeatherContext);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (city.trim() === "") return;
     handleCityName(city);
+    navigate('/');
     setCity("");
   };
 
@@ -22,7 +25,11 @@ const SearchBar = () => {
         className="text-black w-full max-w-md px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         aria-label="Search city"
       />
-      <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded-r-md hover:bg-blue-700 focus:ring-2 focus:ring-blue-600:" aria-label="Search">
+      <button
+        type="submit"
+        className="px-4 py-2 bg-blue-600 text-white rounded-r-md hover:bg-blue-700 focus:ring-2 focus:ring-blue-600:"
+        aria-label="Search"
+      >
         Search
       </button>
     </form>
