9de786ab8676a3bdfad1287e2009a42aa516e0a8
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-10-27 06:35:32 +0600

    feat(Favorites): add button replacing li to display favorite cities

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,34 +1,36 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { WeatherContext } from "../context/WeatherContext";
 import { useNavigate } from "react-router-dom";
 
 const Favorites = () => {
-    const {favorites, fetchWeather, removeFavorite} = useContext(WeatherContext);
-    const navigate = useNavigate();
+  const { favorites, fetchWeather, removeFavorite } =
+    useContext(WeatherContext);
+  const navigate = useNavigate();
 
-    if (favorites.length === 0) return (<p>No favorites added</p>)
+  if (favorites.length === 0) return <p>No favorites added</p>;
 
-    return (
+  return (
+    <div>
+      <h2>Favorites</h2>
+      <div>
         <div>
-            <h3>Favorites</h3>
-            <ul>
-                {favorites.map((city, index) => (
-                    <li key={index}>
-                        <button onClick={ async () => {
-                            await(fetchWeather(city));
-                            navigate('/')
-                        }}>
-                            {city}
-                        </button>
-                        <button onClick={() => removeFavorite(city)}>
-                            Remove
-                        </button>
-
-                    </li>
-                ))}
-            </ul>
+          {favorites.map((city, index) => (
+            <div key={index}>
+              <button
+                onClick={async () => {
+                  await fetchWeather(city);
+                  navigate("/");
+                }}
+              >
+                {city}
+              </button>
+              <button onClick={() => removeFavorite(city)}>&times;</button>
+            </div>
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Favorites;
