5383577807821d33ac81784ec3c1397963839e90
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-10-27 23:00:04 +0600

    style(SearchBar): remove focus styling with ring

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -22,12 +22,12 @@ const SearchBar = () => {
         value={city}
         onChange={(e) => setCity(e.target.value)}
         placeholder="Enter city name"
-        className="text-black w-full max-w-md px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="text-black w-full max-w-md px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none"
         aria-label="Search city"
       />
       <button
         type="submit"
-        className="px-4 py-2  text-white rounded-r-md bg-gray-600 hover:bg-gray-700 focus:ring-2 focus:ring-blue-600:"
+        className="px-4 py-2  text-white rounded-r-md bg-gray-600 hover:bg-gray-700"
         aria-label="Search"
       >
         Search
